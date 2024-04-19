c9a06507cdb442e4c075c7346296a38bf207bee4
macros77 <pawel.bialic@gmail.com>
2024-04-19 16:30:48 +0200

    Update project.v

--- src/project.v
+++ src/project.v
@@ -16,66 +16,59 @@ module tt_um_macros77_subneg (
     input  wire       rst_n     // reset_n - low to reset
 );
 
-    assign uio_out = 0;
-    assign uio_oe  = 0;
-
     wire reset = ! rst_n;
+    
+    reg [3:0] state = 0;
+    reg [7:0] PC = 0;
+    reg [7:0] addrA = 0;
+    reg [7:0] addrB = 0;
+    reg [7:0] addrC = 0;
+    reg [7:0] valA = 0;
+    reg [7:0] valB = 0;
 
-    reg [4:0] memory[21:0];
-   
-    reg [1:0] state = 0;
-    reg [4:0] PC = 0;
-    reg [4:0] addrA = 0;
-    reg [4:0] addrB = 0;
-    reg [4:0] addrC = 0;
-    reg [4:0] valA = 0;
-    reg [4:0] valB = 0;
+    assign uio_out[7:3] = 0;
 
-    reg [7:0] display = 0;
-    assign uo_out = display;
+    reg LE = 1;
+    reg MOE = 0;
+    reg MWE = 0;
+    assign uio_out[0] = LE;
+    assign uio_out[1] = MOE;
+    assign uio_out[2] = MWE;
+
+    reg [7:0] bus_dir = 1;
+    assign uio_oe  = bus_dir;
+    reg [7:0] bus_data_out = 0;
+    assign uio_out = bus_data_out;
+    reg [7:0] bus_data_in = 0;
+    assign bus_data_in = uio_in;
     
     always@(posedge clk) begin
       if (reset) begin
         PC <= 0;
-        state <= 0;
-        memory[0] <=	18;
-        memory[1] <=	18;
-        memory[2] <=	3;
-        memory[3] <=	19;
-        memory[4] <=	20;
-        memory[5] <=	0;
-        memory[6] <=	20;
-        memory[7] <=	18;
-        memory[8] <=	9;
-        memory[9] <=	18;
-        memory[10] <=	21;
-        memory[11] <=	12;
-        memory[12] <=	18;
-        memory[13] <=	18;
-        memory[14] <=	15;
-        memory[15] <=	19;
-        memory[16] <=	18;
-        memory[17] <=	0;
-        memory[18] <=	0;
-        memory[19] <=	1;
-        memory[20] <=	29;
-        memory[21] <=	31;       
+        state <= 0;      
       end
       case (state)
         0: begin
-            addrA <= memory[PC];
-            addrB <= memory[PC+1];
-            addrC <= memory[PC+2];
+            bus_data <= PC;
+            bus_dir <= 1;
+            LE <= 1;
+            MOE <= 0;
+            MWE <= 0;
             state <= 1;
         end
         1: begin
-            valA <= memory[addrA];
-            valB <= memory[addrB];         
+            LE <= 0;
+            MOE <= 1;
+            MWE <= 0;
             state <= 2;
         end
         2: begin
-            if (addrB==21) display <= (valB - valA); 
-            else memory[addrB] <= (valB - valA);           
+            A <= bus_data_in;
+            state <= 3;
+        end          
+        3: begin
+//            if (addrB==21) display <= (valB - valA); 
+//            else memory[addrB] <= (valB - valA);           
             if (valA>valB) PC <= addrC;
             else PC <= PC + 3;
             state <= 0;
