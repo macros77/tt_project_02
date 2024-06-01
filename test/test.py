285c95bb1986c52409a8f5fb46acb980f0bea1b0
macros77 <pawel.bialic@gmail.com>
2024-06-01 18:03:38 +0200

    Update test.py

--- test/test.py
+++ test/test.py
@@ -45,17 +45,18 @@ async def test_project(dut):
     assert dut.uio_out.value == 0
 
     await ClockCycles(dut.clk, 1)
-    assert dut.uio_in.value = 10
+    dut.uio_in.value = 10
     await ClockCycles(dut.clk, 3)
     assert dut.uio_out.value == 1
 
     await ClockCycles(dut.clk, 1)
-    assert dut.uio_in.value = 11
+    dut.uio_in.value = 11
     await ClockCycles(dut.clk, 3)
     assert dut.uio_out.value == 2
 
     await ClockCycles(dut.clk, 1)
-    assert dut.uio_in.value = 12
+    dut.uio_in.value = 12
+    await ClockCycles(dut.clk, 3)
 
     # Keep testing the module by changing the input values, waiting for
     # one or more clock cycles, and asserting the expected output values.
